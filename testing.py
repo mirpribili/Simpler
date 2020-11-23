2f35068306b4713b602dd10eec4958a827f54bdf
Mirpribili <mirpribili@ya.ru>
2020-11-23 18:26:35 +0300

    replace readme

--- testing.py
+++ testing.py
@@ -2,25 +2,26 @@
 # -*- coding: utf-8 -*-
 
 """
-Затронутые темы:
-    - реляционные базы данных
-    - декораторы
-    - классы
-    - генераторы
-    - single ton
-    - ну и мелочи
-    - - отсутствие дублирование кода при работе с 5 бд
-    - - случайное смешивание списка
-    - - избавление от дублирования в бд (решение проблемы вставки в бд имеющегося в ней значения)
-Реализованный функционал:
-    - добавка в бд правил с тегами метками и примерами
-    - добавка в бд тестов с тегами метками и примерами
-    - вывод правил
-    - - всех
-    - - по определенному тегу
+    Затронутые темы:
+        - реляционные базы данных
+        - декораторы
+        - классы
+        - генераторы
+        - single ton
+        - ну и мелочи
+        - - отсутствие дублирование кода при работе с 5 бд
+        - - случайное смешивание списка
+        - - избавление от дублирования в бд (решение проблемы вставки в бд имеющегося в ней значения)
+    Реализованный функционал:
+        - добавка в бд правил с тегами метками и примерами
+        - добавка в бд тестов с тегами метками и примерами
+        - вывод правил
+        - - всех
+        - - по определенному тегу
 """
 import random
 from random import randrange
+
 
 def print_all_rules_with_comment(method_to_decorate):
     def wrapper(*args, **kwargs):  # self, rules=rules
@@ -36,8 +37,8 @@ def print_all_rules_with_comment(method_to_decorate):
             if i % 5 == 0 and i > 0:
                 input()
             for j, element in enumerate(rules_examples_and_outher):
-                print("\t" + text[j] + element)
-            print("\t. " * 15)
+                print("\t" + text[j] + element.replace(";;", "\n\t\t\t"))
+            print(">",i+1, "\t. " * 14)
         print("-" * 30)
         print("Правила закончились")
         print("=" * 30)
@@ -93,7 +94,7 @@ class Singleton_BD(object):
 
     @print_all_rules_with_comment
     def print_rules(self, rules="all"):
-        if rules is "all":
+        if rules == "all":
             rules = self.general_rules
         for ids in rules:
             yield [
@@ -103,20 +104,44 @@ class Singleton_BD(object):
                 self.marks[ids["id_mark"]],
             ]
 
-    def print_tagged_rules(self, tag):
-        # print(self.general_rules)
+    def __for_deep_search_tegged(self, s_tag):
+        true_tags = set()
+        for index, tag in enumerate(self.tags):
+            if s_tag in tag.split(' '):
+                true_tags.add(index)
+        return true_tags
+
+    def print_tagged_rules(self, tag, deep=0):
+        # задекорируй меня
+        print(f"Ищу по тегу \"{tag}\"")
+        id_tags = set()
+
         if tag in self.tags:
-            id_tags = self.tags.index(tag)
+            id_tags.add(self.tags.index(tag))
+        if deep: # глубокий но для односложных запросов
+            assert len(tag.split(' ')) < 2, "print_tagged_rules -> deep_search_teged"
+            #  а как will be ------ WILL + be
+            id_tags.update(self.__for_deep_search_tegged(tag))
+        
+
+
+        if len(id_tags):
             rules = list()
             for rule in self.general_rules:
-                if id_tags in rule["ids_tags"]:
-                    rules.append(rule)
+                for id_tag in id_tags:
+                    if id_tag in rule["ids_tags"] and rule not in rules:
+                        rules.append(rule)
             self.print_rules(rules)
         else:
-            return "Тег не найден"
+            # задекорируй меня
+            print( f"\tТег \"{tag}\" не найден" )
 
     def print_all_tags_yeld():
         pass
+
+    def deep_search_teged():
+        pass
+        #"would + if + be" -> as "be"
 
     def print_tag_rules():
         pass
@@ -162,24 +187,212 @@ class Singleton_BD(object):
 s = Singleton_BD()
 
 s.add_rule(
-    rule="если объект САМ СОВЕРШАЕТ действие " +
-         "{прилагательное = глагол + ING} " +
-         "звук РАЗДРАЖАЕТ вас = раздражающий annoyING",
+    rule="если объект САМ СОВЕРШАЕТ действие;; " +
+         "{прилагательное = глагол + ing};; " +
+         "звук РАЗДРАЖАЕТ вас = раздражающий annoying",
     examples=["what an embrassing situation?", "who's dating who?"],
-    tags=["отглагольные прилагательные", "глагол + ING", "V + ING", "#Up.Interm.V.ing"],
+    tags=["отглагольные прилагательные", "глагол + ing", "V + ing", "#Up.Interm.V.ing"],
     mark="#Up.Interm.V.ing"
 )
 s.add_rule(
-    rule="если объект ПОДВЕРГАЕТСЯ воздействию " +
-         "{прилагательное = глагол + ED (3ф.)} " +
-         "вас РАЗДРАЖАЕТ звук = вы раздражены annoyED",
-    examples=["brokEN", "relaxED"],
-    tags=["отглагольные прилагательные", "глагол + ED", "V + ED", "#Up.Interm.V.ed"],
+    rule="если объект ПОДВЕРГАЕТСЯ воздействию;; " +
+         "{прилагательное = глагол + ed (3ф.)};; " +
+         "вас РАЗДРАЖАЕТ звук = вы раздражены annoyed",
+    examples=["brokEN", "relaxed"],
+    tags=["отглагольные прилагательные", "глагол + ed", "V + ed", "#Up.Interm.V.ed"],
     mark="#Up.Interm.V.ed"
 )
-s.print_rules()
 
-s.print_tagged_rules("V + ED")
+s.add_rule(
+    rule="Сослагательное наклонение образ. с пом. {would - БЫ};; " +
+         "{вопрос: Wh. would + подлеж. + глагол? Отриц: wouldn't};; " +
+         "C мест-ем сокращ до {'d};;" +
+         "{ if } условие при котором СЕЙСЧАС НАСТУПИЛО БЫ событ.;;" +
+         "{if + усл. в прош. вр. +[be -> were] + событие с would}",
+    examples=["I would do it for you - я !с!делаю это для тебя"],
+    tags=["Сослагательное наклонение", "would", "would + if", "would + if + be", "Present.would",  "#Interm.would"],
+    mark="#Interm.would"
+)
+
+s.add_rule(
+    rule="Вероятность событий. Если событие ВПОЛНЕ может быть, то may | might | could;;" +
+         "{подлеж. + may | might | could  + глагол} ",
+    examples=["it might rain tomorrow - завтра может пойти дождь"],
+    tags=["Вероятность событий", "событие ВПОЛНЕ может быть", "may might could", "may", "might", "could",  "#Interm.may.might.could"],
+    mark="#Interm.may.might.could"
+)
+
+s.add_rule(
+    rule="Вероятность событий. Слова, обозначающие уверенность: should | must - должны, can't - не может;;" +
+         "{подлеж. + should (shouldn't) | must | can't  + глагол} ;;" +
+         "Вопрос: {should подлеж. + глагол} ",
+    examples=[""],
+    tags=["Вероятность событий", "Слова, обозначающие уверенность", "should must can't", "should", "must", "can't",  "#Interm.should.must.can't"],
+    mark="#Interm.should.must.can't"
+)
+
+# Tenses------------------------
+'''
+s.add_rule(
+    rule="Pr.S. действие происходит обычно / в принципе;;" +
+         "{+:_ V,(he)V s; -:_ don't(he)doesn't V};;" +
+         "{+:_ have|has; -:_ don't(he)doesn't have };;" +
+         "{+:_ am|is|are|can; -:_ am|is|are|can not };;" +
+         "Собираться что-то делать;;" +
+         "{+:Wh. am|is|are _ going to V};;" +
+         "Следует, стоит;;" +
+         "{+:_ should V; -:_ shouldn't V};;" +
+         "Обязан;;" +
+         "{+:_ have|has to V; -:_ don't|doesn't have|has to V};;";
+    examples=["I work. She has a work*. We don't work. He doesn't work"],
+    tags=["Pr.S", "V s", "does", "do", "have", "has", "am are is", "can", "am going to", "am going to", "ing", "should V", "have to V", ],
+    mark="#Elem.Pr.S.do.does.Vs.have.am.are.is.can.going.to"
+)
+'''
+s.add_rule(
+    rule="Pr.S. действие происходит обычно / в принципе;;" +
+         "{+:_ V,(he)V s; -:_ don't(he)doesn't V};;" +
+         "{+:_ have|has; -:_ don't(he)doesn't have };;" +
+         "{+:_ am|is|are|can; -:_ am|is|are|can not };;",
+    examples=["I work. She has a work*. We don't work. He doesn't work"],
+    tags=["Pr.S", "V s", "does", "do", "have", "has", "am are is", "can",],
+    mark="#Elem.Pr.S.do.does.Vs"
+)
+
+s.add_rule(
+    rule="Собираться что-то делать;;" +
+         "{+:Wh. am|is|are _ going to V};;" ,
+    examples=[""],
+    tags=["am going to", "am going to", "ing",  ],
+    mark="#Elem.going.to"
+)
+
+s.add_rule(
+    rule="Следует, стоит;;" +
+         "{+:_ should V; -:_ shouldn't V};;",
+    examples=[""],
+    tags=["should V", ],
+    mark="#Elem.should"
+)
+s.add_rule(
+         "Обязан;;" +
+         "{+:_ have|has to V; -:_ don't|doesn't have|has to V};;",
+    examples=[""],
+    tags=["have to V", ],
+    mark="#Elem.have.to"
+)
+
+s.add_rule(
+    rule="Должен сделать;;" +
+         "{+:_ must V; -:_ mustn't V};;" ,
+    examples=[""],
+    tags=["must V"],
+    mark="#Elem.must"
+)
+
+
+s.add_rule(
+    rule="P.S. действие произошло в завершенный промежуток времени;;" +
+         "{+:_ V ed; -:_ did not V };;" +
+         "{+:_ was|were|could; -:_ was|were|could not };;" +
+         "{  ? string ? [yesterday | last week | once | two days ago] };;" +
+         "Действие было в планах;;" +
+         "{+:Wh. was|were _ going to V };;" +
+         "Способности, возможности в прошлом;;" +
+         "{ could V, couldn't V };;"  +
+         "Я мог мне удолось;;"  +
+         "{ _ was|were able to V};;" ,
+    examples=["It was cold. We didn't buy tea. We bought tea. We washed the window.*"],
+    tags=["P.S", "was", "were", "yesterday", "ing", "V ed", "V ed", "could V", "was were able to V"],
+    mark="#Elem.P.S.was.were.could"
+)
+s.add_rule(
+    rule="F.S. действие случится в будущем;;" +
+         "{+:_ will V; -:_ won't V };;" +
+         "При каком условии наступит событ. в будущем;;" +
+         "{if + усл. в наст. вр. + событие в будущем};;" +
+         " Я смогу, я буду уметь ;;" +
+         "{+:_ will be able to; -:_ won't be able to}",
+    examples=[""],
+    tags=["F.S", "will", "will if", "will br able to"],
+    mark="#Elem.F.S.will"
+)
+
+s.add_rule(
+    rule="Pr.C. Совершается прямо сейчас и еще не завершилось;;" +
+         "{+:_ am|is|are V ing; -:_ am|is|are not V ing };;" ,
+    examples=[""],
+    tags=["Pr.C", "V ing", "was V ing", "ing"],
+    mark="#Elem.Pr.C"
+)
+
+s.add_rule(
+    rule="P.C. одно действие было прервано другим;;" +
+         "{+:_ was|were V ing; -:_ was|were not V ing };;" ,
+    examples=[""],
+    tags=["P.C", "V ing", "was V ing", "were V ing", "ing"],
+    mark="#Elem.P.C"
+)
+
+s.add_rule(
+    rule="F.C. будет происходить и в указанный момент не завершится;;" +
+         "{+:_ will be V ing; -:_ won't be V ing };;" +
+         "{+:Will there be _ ?; };;" ,
+    examples=[""],
+    tags=["F.C", "V ing", "will be V ing", "will V ing", "be V ing", "ing"],
+    mark="#Elem.F.C"
+)
+
+s.add_rule(
+    rule="Pr.P. (не было) сделано к настоящему моменту (не важно когда!);;" +
+         "{+: _ have|has V ed; -:_ have|has not V ed };;" ,
+    examples=[""],
+    tags=["have V ed", "has V ed", "have ed", "ed", "V ed"],
+    mark="#Interm.Pr.P"
+)
+
+s.add_rule(
+    rule="Pr.P.C. Действие длится неколторое время и продолжается сейчас;;" +
+         "{+: _ have|has been V ing; -:_ have|has been not V ing };;" ,
+    examples=[""],
+    tags=["have been V ing", "have been V ing"],
+    mark="#Interm.Pr.P.C."
+)
+
+
+s.add_rule(
+    rule="P.P. Действие завершилось РАНЕЕ другого события в прошлом;;" +
+         "{+: _ had V ed; -:_ hadn't V ed };;" ,
+    examples=[""],
+    tags=["had V ed", "hadn't V ed"],
+    mark="#Interm.P.P."
+)
+
+s.add_rule(
+    rule="F.P. Действие завершится к указанному моменту;;" +
+         "{+: _ will have V ed; -:_ won't have V ed };;" +
+         "{by the time };;" ,
+    examples=[""],
+    tags=["will have V ed",],
+    mark="#Interm.F.P."
+)
+
+s.add_rule(
+    rule="it be. Нельзя определить кто выполняет действие;;" +
+         "{it is|was ...; it will be ...};;" ,
+    examples=[""],
+    tags=["it be", "it.be"],
+    mark="#Elem.it.be"
+)
+
+
+
+#s.print_rules()
+
+#s.print_tagged_rules("have")
+#s.print_tagged_rules("ed",deep=1)
+while True:
+    s.print_tagged_rules(input("Enter you word:"),deep=1)
 
 '''
 print("Object created", s, s.print_arg())
