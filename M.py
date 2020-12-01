fbdc55c68b583500342226a2a827d1680f00c5d0
Mirpribili <mirpribili@ya.ru>
2020-12-01 13:49:30 +0300

    replace readme

--- M.py
+++ M.py
@@ -1,67 +1,70 @@
-given = [input() for x in range(int(input()))]
-
-question = [input() for x in range(int(input()))]
-
-#print(given)
-#print(question)
-
-
-help1 = False
-
-answer = {}
-
-for line in given:
-    
-    if line.count(':'):
-        child, parents = line.split(':')
-        
-        child = child.split()
-        
-        parents = parents.strip().split()
-        
-        
-        answer[child[0]] = set(parents)
-        
-    else:
-        child = line.split()
-        
-        answer[child[0]] = []
-        
+#вводится первый блок
+classi = {}
+vopros = []
+otvet = []
+if False:
+    a = int(input())
+    b = 0
+    while b < a:
+        c = input().split()
+        vopros.append(c)
+        b += 1
+    print(vopros)
+else:
+    vopros = [['A'], ['B', ':', 'A'], ['C', ':', 'A'], ['D', ':', 'B', 'C']]
 
 
-
-
-# красивый вывод словаря
-for i in answer.items():
-    if help1:print('{} - {}'.format(i[0], ' '.join(i[1])))
-if help1: print('--'*10)
-    
-def search_paren(parent, child):
-    if parent == child: return True
-    
-    result = False
-    for parent_or_not in answer[child]:
-        if result: return True
-        
-        if help1:print('-\tparent:',parent, '\tchild:',child, '\tparent_or_not:',parent_or_not)
-        
-        if parent == parent_or_not:
-            if help1:print('+\tparent:',parent, '\tchild:',child, '\tparent_or_not:',parent_or_not)
-            return True
+#print(vopros)
+#заполнение словаря, вроде работает правильно
+for i in vopros:
+    if len(i) == 1:
+        classi[i[0]] = []
+    elif len(i) == 3:
+        if i[2] in classi.keys():
+            classi[i[2]] += i[0]
         else:
-            result = search_paren(parent,parent_or_not)
-    
-    return result
+            classi[i[2]] = []
+            classi[i[2]] += i[0]
+    elif len(i) > 3:
+        for j in range(2, len(i)):
+            classi[i[j]] = i[0]
 
-    
-    
+print("classi", classi)
+#вводится второй блок
+if False:
+    a1 = int(input())
+    b1 = 0
+    while b1 < a1:
+        c = input().split()
+        otvet.append(c)
+        b1 += 1
+    #вывод результатов проверки
+else:
+    otvet = [['A', 'B'], ['B', 'D'], ['C', 'D'], ['D', 'A']]
+print("otvet", otvet)
 
-
-for line in question:
-    parent, child = line.split()
-    
-    if help1:print(parent, child, end=' : ')
-    if search_paren(parent, child) == True:
-        print('Yes')
+for i in otvet:
+    print(i, "\t", end="")
+    if i[0] in classi.keys() and i[1] in classi.values():
+        print(i, 'Yes')
+    #вот тут нужна помощь
+    #я хочу перебрать в цикле значения, если ключ совпадает, а длина списка в значении больше 1
+    #но он не выводит ответ, так что я видимо не так к ним обращаюсь
+    #запрашиваю помощь!
+    elif i[0] in classi.keys() and len(classi.values()) > 0:
+        for j in classi.values():
+            if i[1] == j:
+                print('Yes')
+            else:
+                print(j)
     else:
         print('No')
+
+
+    for parent in classi:
+        if parent == i[0]:
+            #print(parent)
+            child = classi[parent]
+            if i[1] in child:
+                print("Yes")
+#print(classi)
