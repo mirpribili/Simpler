1167296f56f4a4293e6d5338bb0c781e5d57f5e6
mirpribili <mirpribili@ya.ru>
2021-08-08 19:33:43 +0300

    Add files via upload

--- m_python_2.py
+++ m_python_2.py
@@ -3,7 +3,8 @@ words_per_card = 4
 def ends(string):
     string2 = string.strip()
     if "." in string2:
-        string2 = string2[0:len(string2) - 1]
+        #string2 = string2[0:len(string2) - 1]
+        pass
     return string2
 
 words = []
@@ -14,7 +15,7 @@ ex = []
 ex_str =""
 
 # получим объект файла
-file1 = open("test_4a.txt", "r")
+file1 = open("test_4b.txt", "r")
 
 # считываем все строки
 lines = file1.readlines()
@@ -69,8 +70,16 @@ def func(string):
             string = string[0:len(string) - 1]
         else:
             return string
+def shuffl_string(string):
+    temp = string.split()
+    random.shuffle( temp )
+    return ' '.join(temp)
 
-
+def replace_up(string, what, on_what):
+        temp_text = string.replace(str(what), on_what )
+        word_up = str(what.capitalize())
+        temp_text = temp_text.replace(word_up, on_what )
+        return temp_text
 #print(words)
 #print(texts)
 #print(exs)
@@ -90,20 +99,24 @@ results = []
 for i, word in enumerate(words):
     #print(i)
     result = temp =[]
-    temp = texts[i].split()
-    random.shuffle( temp )
-    result.append(' '.join(temp))
+
     #result.append( word.split(":")[0] )
 
-    #print(result) 
+    #print(result)
+    #----------
+    #result.append(shuffl_string(texts[i]))
+    #result.append(replace_up(texts[i], word.split(":")[0], word.split(":")[1]) )
+    #----------
 
     result.append(texts[i])
+    result.append(replace_up(texts[i], word.split(":")[0], word.split(":")[1].upper() ) )
     result.append(texts[i])
     result.append(exs[i])
     result = '"' + '";"'.join(result) + '"'
     results.append(result)
     #print(result)
 
+random.shuffle( results )
 with open(r"result.txt", "w", encoding="utf-8") as file:
     for  result in results:
         file.write(result + '\n')
